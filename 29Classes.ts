4fe8a989306d0dabae852056842030d5bddac09a
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2026-08-22 13:40:40 +0200

    switch to personal computer

---
+++ 29Classes.ts
@@ -0,0 +1,31 @@
+// WITH A CONSTRUCTOR
+class Person {
+    private name: string;
+    private age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greet() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+}
+
+const person1 = new Person("Alice", 30);
+person1.greet(); // Output: Hello, my name is Alice and I am 30 years old.
+
+
+// WITHOUT A CONSTRUCTOR
+class User {
+    public username : string = "";
+    public email : string = "";
+    public country : string = "";   
+}
+
+const user1 = new User();
+user1.username = "john_doe";
+user1.email = "john_doe@example.com";
+
+console.log(user1.username, user1.email); // Output: john_doe john_doe@example.com
---
+++ 30ClassInheritance.ts
@@ -0,0 +1,53 @@
+// class inheritance example
+class Animal {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    makeSound(): void {
+        console.log(`${this.name} makes a sound.`);
+    }
+}
+
+
+// Dog class inherits from Animal class
+// Use super keyword to call the constructor of the parent class
+class Dog extends Animal {
+    breed: string;
+    
+    constructor(name: string, breed: string) {
+        super(name);
+        this.breed = breed;
+    }
+
+    makeSound(): void {
+        console.log(`${this.name} barks.`);
+    }
+}
+
+
+// Cat class inherits from Animal class
+// Use super keyword to call the constructor of the parent class
+class Cat extends Animal {
+    color: string;
+
+    constructor(name: string, color: string) {
+        super(name);
+        this.color = color;
+    }
+
+    makeSound(): void {
+        console.log(`${this.name} meows.`);
+    }
+}
+
+
+// Creating instances of Dog and Cat
+const dog = new Dog("Buddy", "Golden Retriever");
+const cat = new Cat("Whiskers", "Tabby");
+
+// Calling makeSound method on Dog and Cat instances
+dog.makeSound();
+cat.makeSound();
